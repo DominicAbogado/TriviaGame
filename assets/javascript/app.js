7e2930dabc232fce8a296ef152f6d85c3466c34f
DominicAbogado <dominic.j.abogado@gmail.com>
2018-05-05 22:14:34 -0400

    Added Timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,33 @@
 $(document).ready(function () {
 
+    var number = 100;
+
+    var intervalId;
+
+    $("#stop").on("click", stop);
+
+    $("#resume").on("click", run);
+
+    function run() {
+        clearInterval(intervalId);
+        intervalId = setInterval(decrement, 1000);
+    }
+
+    function decrement() {
+
+        number--;
+
+        $("#show-number").html("<h2>" + number + "</h2>");
+
+        if (number === 0) {
+            stop();
+            alert("Time Up!");
+        }
+    }
+
+    function stop() {
+        clearInterval(intervalId);
+    }
+
+    run();
 });
